af9d5d4cac4006c0f9496ac86509c66eb8949c56
David de Meij <daviddemeij@gmail.com>
2023-04-09 09:53:25 +0200

    Change default size to tile-regular to avoid error

--- python_picnic_api/helper.py
+++ python_picnic_api/helper.py
@@ -34,7 +34,7 @@ def get_recipe_image(id: str, size: str = "regular"):
     return f"{IMAGE_BASE_URL}/recipes/{id}/{size}.png"
 
 
-def get_image(id: str, size: str = "regular", suffix: str = "webp"):
+def get_image(id: str, size: str = "tile-regular", suffix: str = "webp"):
     assert (
         "tile" in size if suffix == "webp" else True
     ), "webp format only supports tile sizes"
